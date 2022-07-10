b04300854ea34b5bcf895aebc56e2283e1757d71
Ryan <drighem.rayan@hotmail.com>
2022-07-10 17:19:10 +0200

    First commit

---
+++ css/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Students {
+    char name[25];
+    char Lname[25];
+    int age;
+    float height;
+    char grades[5];
+    char address[100];
+} Student
+
+int main(){
+    return 0;
+}
